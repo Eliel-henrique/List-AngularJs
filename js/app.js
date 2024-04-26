8f80f7d5a680e00b589189fbed73cea3ac23a7f8
Eliel Henrique <elielhenriquedepaula07@gmail.com>
2024-04-26 16:51:08 -0300

    Primeiro commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var app = angular.module("myShoppingList", []);
+app.controller("myCtrl", function($scope){
+  $scope.produtos = ["Milk", "Bread", "Cheese"];
+  $scope.addItem = function(){
+    if($scope.produtos.indexOf($scope.item) == -1){
+      $scope.produtos.push($scope.item);
+    } else{
+      $scope.errortext = "Esse item já esté em sua lista";
+      console.log($scope.errortext);
+    }
+  }
+  $scope.removeItem = function(x){
+    $scope.produtos.splice(x,1)
+  }
+});
