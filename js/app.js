9919761a0d9894eeafcee2ba3b476f6c512541f0
Eliel Henrique <elielhenriquedepaula07@gmail.com>
2024-05-06 17:25:52 -0300

    mudando estilização e melhorando a validação

--- js/app.js
+++ js/app.js
@@ -1,15 +1,29 @@
+
 var app = angular.module("myShoppingList", []);
-app.controller("myCtrl", function($scope){
-  $scope.produtos = ["Milk", "Bread", "Cheese"];
-  $scope.addItem = function(){
-    if($scope.produtos.indexOf($scope.item) == -1){
+
+app.controller("myCtrl", function($scope) {
+  $scope.produtos = ["Leite", "Pão", "Queijo"];
+  
+  $scope.addItem = function() {
+    if(!$scope.item){
+      $scope.errortext = "Por favor, insira um item antes de adicionar à lista.";
+      alert($scope.errortext)
+      return;
+    }
+    if ($scope.produtos.indexOf($scope.item) == -1) {
       $scope.produtos.push($scope.item);
-    } else{
-      $scope.errortext = "Esse item já esté em sua lista";
+      $scope.item = "";
+    } else {
+      $scope.errortext = "Esse item já está em sua lista";
       alert($scope.errortext);
     }
-  }
-  $scope.removeItem = function(x){
-    $scope.produtos.splice(x,1)
-  }
+  };
+  
+  $scope.removeItem = function(x) {
+    $scope.produtos.splice(x, 1);
+    // Adicionando o alerta aqui
+    alert('Item removido com sucesso!');
+  };
+
+  
 });
